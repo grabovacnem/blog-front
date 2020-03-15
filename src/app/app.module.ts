cad517edc4cf0eed7bab21307a0fa56b6107c8ac
Nemanja Grabovac <grabovacnem@gmail.com>
2020-03-15 13:34:47 +0100

    Created header and sidebar components. Added general flex classes and styled header.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BlogComponent } from './blog/blog.component';
 import { AddEditComponent } from './blog/add-edit/add-edit.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BlogComponent,
-    AddEditComponent
+    AddEditComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
